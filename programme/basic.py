36dd29490314490deebefc8de61265c821e2946d
duwill <will.y.du@gmail.com>
2019-12-01 16:33:11 +0100

    learning

--- programme/basic.py
+++ programme/basic.py
@@ -4,6 +4,20 @@ polygon = turtle.Turtle()
 
 # num_sides = int(turtle.textinput('Input', 'Input a number of sides:'))
 
+for i in range(5): #[1,2,3,4,5]
+    turtle.forward(100)
+    turtle.right(360/5)
+
+# turtle.forward(100)
+# turtle.right(72)
+# turtle.forward(100)
+# turtle.right(72)
+# turtle.forward(100)
+# turtle.right(72)
+# turtle.forward(100)
+# turtle.right(72)
+# turtle.forward(100)
+# turtle.right(72)
 
 # for i in range():
 
--- programme/polygon.py
+++ programme/polygon.py
@@ -2,16 +2,14 @@ import turtle
 
 polygon = turtle.Turtle()
 
-# num_sides = int(turtle.textinput('Input', 'Input a number of sides:'))
-# side_length = 100
-# angle = 360.0 / num_sides
+num_sides = int(turtle.textinput('Input', 'Input a number of sides:'))
+side_length = 2
+angle = 360.0 / num_sides
 
-# todo add the code here the draw a polygon
 
-# for i in range(num_sides):
-#     polygon.forward(side_length)
-#     polygon.right(angle)
+for i in range(num_sides):
+    polygon.forward(side_length)
+    polygon.right(angle)
 
-# todo end here
 
 turtle.done()
