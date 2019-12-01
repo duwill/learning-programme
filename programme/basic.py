66ccdd6f025ce1736c3f47f2a0ee63325ecdea92
duwill <will.y.du@gmail.com>
2019-12-01 10:21:01 +0100

    learning

---
+++ programme/basic.py
@@ -0,0 +1,13 @@
+import turtle
+
+polygon = turtle.Turtle()
+
+# num_sides = int(turtle.textinput('Input', 'Input a number of sides:'))
+
+
+# for i in range():
+
+
+# todo end here
+
+turtle.done()
